5b67c3dbc87b2b44e9a57022664ff56a19dbfe6c
tapperisa-wq <tapperisa@hotmail.com>
2026-03-26 11:08:14 +0100

    .

---
+++ SanitySandbox/mysandbox/schemaTypes/people.js
@@ -0,0 +1,18 @@
+const people = {
+    name: "people",
+    title: "Personer",
+    type: "document",
+    fields: [
+        {name: "fullname",
+        title: "Fullt navn",
+        type: "string"
+        },
+        {name: "image",
+        title: "Bilde",
+        type: "image"
+        }
+
+    ]
+}
+
+export default people
